8cb20057dbd5aac6f6767823f3d0c91082e1ce6d
vitor <vitor@vitor>
2020-05-19 21:28:36 -0300

    pagina actual

--- php/email.php
+++ php/email.php
@@ -1,59 +1,101 @@
 <?php
-$nombre = $_POST['nombre'];
-$email = $_POST['mail'];
-$telefono = $_POST['telefono'];
-$comentario = $_POST['comentario'];
 
-$body = "<h3><b>Gracias por enviarnos su consulta</b></h3><br><br>"
-        . "<i>En breve nos comunicaremos con usted</i><br><br>"
-        . "<b>Su mensaje fue: </b><br><br>"
-        . "<b>Nombre: </b>" . $nombre . "<br><br>"
-        . "<b>Mail: </b>" . $email . "<br><br>"
-        . "<b>Telefono de contacto: </b>" . $telefono . "<br><br>"
-        . "<b>Consulta: </b>" . $comentario . "<br><br>"
-        . "Recuerde que este es un mensaje generado en forma automatica, no lo responda.<br>"
-        . "En caso de querer enviarnos otro mail hagalo a <b><i>mail calamar</b></i><br>"
-        . "Muchas Gracias!<br>CalamarEnSuTinta.";
-        
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+require("class.phpmailer.php");
+require("class.smtp.php");
 
-require 'src/Exception.php';
-require 'src/PHPMailer.php';
-require 'src/SMTP.php';
 
-$mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = 0;                                       // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'consultashidroflores@gmail.com';           // SMTP username
-    $mail->Password   = 'Hidro2019';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-    //Recipients
-    $mail->setFrom('consultashidroflores@gmail.com', 'Hidroflores');
-    $mail->addAddress($email);     // Add a recipient
-    $mail->addAddress('consultashidroflores@gmail.com');               // Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+$nombre = $_POST["nombre"];
 
-    // Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$email = $_POST["mail"];
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'CONSULTAS HIDROFLORES';
-    $mail->Body    = $body;
-    $mail->send();
-    echo 'Mensaje enviado correctamente';
-} catch (Exception $e) {
-    echo "mensaje incorrecto: {$mail->ErrorInfo}";
+$telefono = $_POST["telefono"];
+
+$mensaje = $_POST["comentario"];
+
+
+$destinatario = "calamarensutintaar@gmail.com";
+
+
+// Datos de la cuenta de correo utilizada para enviar v�a SMTP
+$smtpHost = "mail.calamarensutinta.com.ar";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "info@calamarensutinta.com.ar";  // Mi cuenta de correo
+$smtpClave = "Calamar2019";  // Mi contrase�a
+
+
+
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 587; 
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+// VALORES A MODIFICAR //
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+
+$mail->From = 'info@calamarensutinta.com.ar'; // Email desde donde env�o el correo.
+$mail->FromName = 'CalamarEnSuTinta';
+$mail->AddAddress($destinatario); // Esta es la direcci�n a donde enviamos los datos del formulario
+$mail->AddAddress($email);
+
+$mail->Subject = "Formulario desde el Sitio Web"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "
+<html> 
+
+<body> 
+
+<h1>Gracias por contactarse con Hidroflores</h1>
+
+<p>Su mensaje fue:</p>
+
+<p>nombre: {$nombre}</p>
+
+<p>email: {$email}</p>
+
+<p>telefono: {$telefono}</p>
+
+<p>mensaje: {$mensaje}</p>
+
+<p>Recuerde que este es un mensaje generado en forma automatica, no lo responda.<br>
+    En caso de querer enviarnos otro mail hagalo a <b><i>calamarensutintaar@gmail.com</b></i><br>
+    Muchas Gracias!<br>CalamarEnSuTinta.</p>
+
+</body> 
+
+</html>
+
+<br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n "; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+
+$estadoEnvio = $mail->Send(); 
+if($estadoEnvio){
+    echo "El correo fue enviado correctamente.";
+} else {
+    echo "Ocurri� un error inesperado.";
 }
+
+
+
+
+
+
+
+?>
+
